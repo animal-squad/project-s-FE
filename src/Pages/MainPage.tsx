78765663b8dabf858dfbcdf3dbae56343861936a
Dorae0 <126739831+Dorae0@users.noreply.github.com>
2024-10-16 18:14:15 +0900

    Style: Sidebar 디자인 및 애니메이션 적용
    Sidebar 디자인 구현
    Sidebar 열릴 때 / 닫힐 때 애니메이션 구현
    동적으로 Main Page 들어올 부분이 움직일 수 있도록 구현
    
    #4

--- src/Pages/MainPage.tsx
+++ src/Pages/MainPage.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { Example } from "../Layout/Sidebar";
 
 const MainPage = () => {
   return (
-    <div className="flex items-center justify-center w-screen v-screen w-full">
-      <label className="block text-primary_text text-xl font-bold mb-2 flex justify-center">
-        Main Page
-      </label>
+    <div className="flex h-screen">
+      <Example />
+      <div className="flex flex-grow items-center justify-center">
+        <div className="flex justify-center w-full">
+          <label className="block text-primary_text text-xl font-bold mb-2">
+            Main Page
+          </label>
+        </div>
+      </div>
     </div>
   );
 };
