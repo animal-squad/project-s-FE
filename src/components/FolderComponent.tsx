a699f082350aea9f049b6384c3b81a6876beaf7c
Dorae0 <126739831+Dorae0@users.noreply.github.com>
2024-11-06 17:24:09 +0900

    Feat: Storage > 바구니 명칭 변경 및 Sidebar 버튼 기능 변경
    명칭 변경 : storage -> bucket
    endPoint 변경 : storage -> bucket
    sidebar 아이콘 변경 : folder -> bucket
      sidebar 전용 아이콘 추가 : Bucket_Icon_Bold
    Bucket_Icon.svg 변경 (배경 투명으로)
    StoragePage, StorageHeader.tsx 명칭 변경 : BucketPage, BucketHeader
    
    #38

--- src/components/FolderComponent.tsx
+++ src/components/FolderComponent.tsx
@@ -1,6 +1,6 @@
 // FolderComponent.tsx
 import React from "react";
-import { FaFolder } from "react-icons/fa";
+import { ReactComponent as Bucket_Icon } from "../../public/assets/icons/Bucket_Icon.svg";
 
 interface FolderComponentProps {
   name: string;
@@ -15,10 +15,12 @@ const FolderComponent: React.FC<FolderComponentProps> = ({ name, onClick }) => {
       onClick={onClick} // 클릭 이벤트 처리
     >
       <div className="w-16 h-12 bg-yellow-300 rounded-md mb-2"></div>
-      <FaFolder color="ebc351" size="30" />
+      <Bucket_Icon width="60" height="60"/>
       <span className="text-sm text-center text-primary_text">{name}</span>
     </div>
   );
 };
 
 export default FolderComponent;
+
+//"/assets/icons/Bucket_Icon.svg";
